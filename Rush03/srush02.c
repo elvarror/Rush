f6f2693112d74d7078859e6a778059d532734daf
elvarror <77893913+elvarror@users.noreply.github.com>
2021-02-14 22:03:38 +0200

    Create srush02.c
    vsimeono@basecamp:~/Back-Up/ex00$ norminette -R CheckForbiddenSourceHeader rush02.c
    Norme: rush02.c

---
+++ Rush03/srush02.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsimeono <vsimeono@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/14 20:00:26 by vsimeono          #+#    #+#             */
+/*   Updated: 2021/02/14 20:00:27 by vsimeono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	loop_putchar(char l, char m, char n, int x)
+{
+	int i;
+
+	i = 1;
+	while (i <= x)
+	{
+		if (i == 1)
+			ft_putchar(l);
+		else if (i == x)
+			ft_putchar(m);
+		else
+			ft_putchar(n);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int j;
+
+	j = 1;
+	while (j <= y)
+	{
+		if (j == 1)
+		{
+			loop_putchar('A', 'A', 'B', x);
+		}
+		else if (j == y)
+		{
+			loop_putchar('C', 'C', 'B', x);
+		}
+		else
+		{
+			loop_putchar('B', 'B', ' ', x);
+		}
+		j++;
+	}
+}
